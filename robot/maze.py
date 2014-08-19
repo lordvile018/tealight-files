3da9d453e50daa48f66fbb0043e5662be25760a1
lordvile018 <matt.brks018@gmail.com>
2014-08-19 14:50:03 +0100

    Running robot/maze.py

--- robot/maze.py
+++ robot/maze.py
@@ -8,7 +8,7 @@ from tealight.robot import (move,
 def walk():
   for i in range (0, 1250):
     if touch()=='wall':
-       if right_side=='wall':
+       if right_side()=='wall':
          turn(-45)
          move(1)
        else:
