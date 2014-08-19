512c8d33bb3c78b5c582f96cac92e953f9dc5e5c
lordvile018 <matt.brks018@gmail.com>
2014-08-19 12:04:01 +0100

    Running logo/chessthink.py

--- logo/chessthink.py
+++ logo/chessthink.py
@@ -2,13 +2,17 @@ from tealight.logo import move, turn
 
 def chessboard():
   for i in range(0,4):
-    move(80)
-    turn(90)
+    if i % 2 == 0:
+      move(80)
+      turn(90)
+    else:
+        move(10)
+        turn(-90)
     if i % 2 == 0:
         move(10)
         turn(90)
     else:
         move(10)
-        turn(90)
+        turn(-90)
         
 chessboard()
