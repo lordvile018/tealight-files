5f5ec9767aaf6afe57023abbd5f220d21885fcf9
lordvile018 <matt.brks018@gmail.com>
2014-08-19 12:02:18 +0100

    Running logo/chessthink.py

--- logo/chessthink.py
+++ logo/chessthink.py
@@ -4,10 +4,10 @@ def chessboard():
   for i in range(0,1):
     move(80)
     turn(90)
-      if i % 2 == 0:
+    if i % 2 == 0:
         move(10)
         turn(90)
-      else:
+    else:
         move(10)
         turn(-90)
         
