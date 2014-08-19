ee000861569a0d199ca6df7ed9ea84994f1011f4
lordvile018 <matt.brks018@gmail.com>
2014-08-19 15:10:36 +0100

    Running robot/refuel.py

--- robot/refuel.py
+++ robot/refuel.py
@@ -5,5 +5,14 @@ from tealight.robot import (move,
                             smell, 
                             left_side, 
                             right_side)
-fo
+for i in range (0, 400):
+  for j in range (0,3)
+    look()
+    if look()=='fruit':
+      while touch() !='fruit':
+        move()
+      move()  
+    else:
+      turn(1)
+    
 # Add your code here
