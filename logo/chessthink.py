24aee7736f6cdd41571e2cd7d037991e77eb253a
lordvile018 <matt.brks018@gmail.com>
2014-08-19 12:25:09 +0100

    Running logo/chessthink.py

--- logo/chessthink.py
+++ logo/chessthink.py
@@ -35,30 +35,30 @@ def shading():
   for i in range(0,8):
     if i % 2 == 0:
       move(10)
-      turn(90)
+      turn(-90)
     else:
         move(10)
-        turn(-90)
+        turn(90)
     if i % 2 == 0:
         move(1)
-        turn(90)
+        turn(-90)
     else:
         move(1)
-        turn(-90)
+        turn(90)
   move(10)
-  turn(-90)     
+  turn(90)     
   for i in range(0,8):
     if i % 2 == 0:
       move(10)
-      turn(90)
+      turn(-90)
     else:
       move(10)
-      turn(-90)
+      turn(90)
     if i % 2 == 0:
       move(-1)
-      turn(90)
+      turn(-90)
     else:
       move(-1)
-      turn(-90)
+      turn(90)
   move(10)
 chessboard()
