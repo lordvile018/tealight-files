97b4d390144dd1ea6edf1950fbce2854be5dfd91
lordvile018 <matt.brks018@gmail.com>
2014-08-19 15:26:54 +0100

    Running robot/chess.py

--- robot/chess.py
+++ robot/chess.py
@@ -5,7 +5,7 @@ from tealight.robot import (move,
                             smell, 
                             left_side, 
                             right_side)
-for i in range (0,700)
+for i in range (0,700):
   if look()=='fruit':
     while touch()!= 'fruit':
       move()
