b8064f66eb9bdd138429a3ab22ac5ee8e1a696ca
lordvile018 <matt.brks018@gmail.com>
2014-08-20 12:29:53 +0100

    Running art/racetrack.py

--- art/racetrack.py
+++ art/racetrack.py
@@ -1,5 +1,13 @@
-from tealight.art import (line)
-def car():
-  line(-10,10,-20,20)
-  line(-20,20,-15,15)
-  line(-15,15,-10,10)
+def handle_keydown(key):
+  if key='left':
+    currentDirection= currentDirection- 3
+  elif key='right':
+    currentDirection= currentDirection +3
+  elif key= 'up':
+    Acc= Acc+2
+  elif key= 'down':
+    Acc= Acc-2
+def handle_keyup(key):
+   if key='up':
+      Acc=Acc-1
+Vel= Vel + Acc
