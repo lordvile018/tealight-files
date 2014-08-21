9be5070591e04852e8e4322f05fc3c89b749f148
lordvile018 <matt.brks018@gmail.com>
2014-08-21 10:25:40 +0100

    Running art/racetrack.py

--- art/racetrack.py
+++ art/racetrack.py
@@ -1,13 +1,13 @@
 def handle_keydown(key):
-  if key='left':
+  if key=='left':
       currentDirection= currentDirection- 3
   elif key='right':
     currentDirection= currentDirection +3
-  elif key= 'up':
+  elif key== 'up':
     Acc= Acc+2
   elif key= 'down':
     Acc= Acc-2
 def handle_keyup(key):
-   if key='up':
+   if key=='up':
       Acc=Acc-1
 Vel= Vel + Acc
