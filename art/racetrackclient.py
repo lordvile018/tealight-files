a8d65f1ebd87f5be6f6e0572bfc743cf34eaf3a2
lordvile018 <matt.brks018@gmail.com>
2014-08-21 11:34:20 +0100

    Running art/racetrackclient.py

--- art/racetrackclient.py
+++ art/racetrackclient.py
@@ -4,8 +4,8 @@ import random
 userId= int(tealight.utils.now()) +random.randint()
 connect("racetracktest")
 send("connected")
-def regisrtation_handler(message):
-  for i in range (0, carNumber):
+#def regisrtation_handler(message):
+  #for i in range (0, carNumber):
   #call draw car function and increment placement
   #horizontally? also change colour
 send (userId)
