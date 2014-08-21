1cab81174caff85362a314c31aa15b7dc8405c48
lordvile018 <matt.brks018@gmail.com>
2014-08-21 11:12:00 +0100

    Running art/racetrack.py

--- art/racetrack.py
+++ art/racetrack.py
@@ -1,15 +1,16 @@
 
-def handle_keydown(key):
+def direction_handle_keydown(key, currentDirection):
   global Acc, currentDirection
   if key=='left':
-      currentDirection= currentDirection- 3
+      return currentDirection- 3
   elif key=='right':
-    currentDirection= currentDirection +3
-  elif key== 'up':
+    return currentDirection +3
+def accelaration_handle_keydown(key):
+  if key== 'up':
     Acc= Acc+2
   elif key== 'down':
     Acc== Acc-2
-def handle_keyup(key):
+def x_handle_keyup(key):
    if key=='up':
       Acc=Acc-1
 def Movement():
